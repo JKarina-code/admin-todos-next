66733b8de38aa9ab2186c02adaa0b61168053948
Jymma <jkmogollon.cp@gmail.com>
2024-05-23 17:23:02 -0500

    Changed credentials in docker-compose

--- src/app/dashboard/products/page.tsx
+++ src/app/dashboard/products/page.tsx
@@ -1,11 +1,12 @@
 import { productsGet } from "@/products/data/products";
 import { ProductCard } from "@/products/components/ProductCard";
+import { Product } from "@/products/interface/product";
 
 async function ProductsPage() {
   const products = await productsGet();
   return (
     <div className="grid grid-cols-1 sm:grid-cols-3 gap-2">
-      {products.map((product: any) => (
+      {products.map((product: Product) => (
         <ProductCard key={product.id} {...product} />
       ))}
     </div>
